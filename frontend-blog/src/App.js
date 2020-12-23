71cf0fb5f72c85e7e5d86b314b363936780df460
Guilherme Andre <guilhermeanm95@hotmail.com>
2020-12-22 23:13:53 -0300

    Consumindo API /users

--- frontend-blog/src/App.js
+++ frontend-blog/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import api from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  state = {
+    users: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('')
+    
+    
+    this.setState({ users: response.data})
+  }
+
+  render(){
+    const { users } = this.state
+
+    return(
+      <div>
+        <h1> Listar usuarios </h1>
+        {console.log(users)}
+        {users.map(user => (
+          <li key={user.id}>
+            <h2><strong>Name:</strong></h2>
+            <a href="#">{user.name}</a>
+          </li>
+        ))}
+      </div>
+    )
+  }
 }
-
 export default App;
